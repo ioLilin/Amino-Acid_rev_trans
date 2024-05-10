e9ad3780e6188d751baba4d90b0e1972d264653e
ioLilin <168093232+ioLilin@users.noreply.github.com>
2024-05-10 17:34:52 +0800

    Windows Forms App
    I build the program by VS2022 C# Windows Forms App. It gives an easy to create a Windows App. So I rewrite my trans program in C#.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Bio_helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.Send = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Your Seq:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(521, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Take Your trans-seq:";
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(12, 72);
+            this.InputBox.Multiline = true;
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(489, 547);
+            this.InputBox.TabIndex = 2;
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(515, 72);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(461, 547);
+            this.OutputBox.TabIndex = 2;
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(441, 629);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(60, 24);
+            this.Send.TabIndex = 3;
+            this.Send.Text = "send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(908, 629);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(68, 22);
+            this.Clear.TabIndex = 4;
+            this.Clear.Text = "clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(998, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.fileToolStripMenuItem.Text = "Files";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(76, 24);
+            this.settingToolStripMenuItem.Text = "Setting";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(138, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(687, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Translate your amino aci sequnece to DNA sequence according to condon usage in Ec" +
+    "oli.";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 665);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bio helper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Bio_helper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string Input;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OutputBox.Clear();
+            InputBox.Clear();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Trans = new Trans();
+            Input = InputBox.Text;
+            Input = Input.ToUpper();
+
+            List<string> dnaseq = new List<string>();
+
+            foreach (char x in Input)
+            {
+                string d = Trans.TRANS(x);
+                dnaseq.Add(d);
+            }
+
+            _ = dnaseq.ToArray();
+            string result = string.Join("", dnaseq);
+          
+            OutputBox.Text = result;
+        }
+        public string GetInputValue()
+        {
+            return Input;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form dlg1 = new Form();
+            dlg1.FormBorderStyle = FormBorderStyle.FixedDialog;
+            dlg1.Size = new Size(700, 300);
+
+            Label lbabout = new Label();
+            lbabout.Text = "Thanks for using this program!\n\nThis program is free and open source. " +
+                "\n\nIf you have any questions or suggestions, please contact me at Github:https://github.com/ioLilin!" +
+                "\n\n感谢使用本程序！本程序为免费开源软件，如果您有任何问题或建议，请通过Github联系我:https://github.com/ioLilin!\n\nGYQ";               
+           // lbabout.Location = new Point(dlg1.Width / 2 - lbabout.Width / 2, dlg1.Height / 2 - lbabout.Height / 2);
+            lbabout.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lbabout.AutoSize = true;
+
+            // 将Label添加到对话框的Controls集合中
+            dlg1.Controls.Add(lbabout);
+
+            dlg1.ShowDialog();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    public class Trans
+    {
+   
+        public string TRANS(char a)
+        {
+            string[] b = new string[0];
+
+            switch (a)
+            {
+                case 'A':
+                    b = Condon.codon_A;
+                    break;
+                case 'G':
+                    b = Condon.codon_G;
+                    break;
+                case 'E':
+                    b = Condon.codon_E;
+                    break;
+                case 'D':
+                    b = Condon.codon_D;
+                    break;
+                case 'V':
+                    b = Condon.codon_V;
+                    break;
+                case 'R':
+                    b = Condon.codon_R;
+                    break;
+                case 'S':
+                    b = Condon.codon_S;
+                    break;
+                case 'K':
+                    b = Condon.codon_K;
+                    break;
+                case 'N':
+                    b = Condon.codon_N;
+                    break;
+                case 'M':
+                    b = Condon.codon_M;
+                    break;
+                case 'I':
+                    b = Condon.codon_I;
+                    break;
+                case 'T':
+                    b = Condon.codon_T;
+                    break;
+                case 'W':
+                    b = Condon.codon_W;
+                    break;
+                case 'C':
+                    b = Condon.codon_C;
+                    break;
+                case 'Y':
+                    b = Condon.codon_Y;
+                    break;
+                case 'L':
+                    b = Condon.codon_L;
+                    break;
+                case 'F':
+                    b = Condon.codon_F;
+                    break;
+                case 'Q':
+                    b = Condon.codon_Q;
+                    break;
+                case 'H':
+                    b = Condon.codon_H;
+                    break;
+                case 'P':
+                    b = Condon.codon_P;
+                    break;
+                default:
+                    b = new string[] { "NNN" };
+                    break;
+            }
+
+            Random random = new Random();    // 使用 Random 类来获取随机索引
+            int randomIndex = random.Next(b.Length);   // 随机索引
+            string c = b[randomIndex];     // 将随机选中的字符串赋值给字符串 c
+            return c;
+        }
+
+    }
+
+    public class Condon
+    {
+        public static string[] codon_A = new string[] { "GCG", "GCG", "GCG", "GCG", "GCG", "GCA", "GCA", "GCA", "GCT", "GCT", "GCC", "GCC", "GCC" };
+
+        public static string[] codon_G = new string[] { "GGG", "GGG", "GGG", "GGT", "GGT", "GGT", "GGC", "GGC", "GGC", "GGC" };
+
+        public static string[] codon_E = new string[] { "GAG", "GAA", "GAA" };
+
+        public static string[] codon_D = new string[] { "GAT", "GAT", "GAC" };
+
+        public static string[] codon_V = new string[] { "GTG", "GTG", "GTG", "GTG", "GTA", "GTA", "GTT", "GTT", "GTT", "GTC", "GTC" };
+
+        public static string[] codon_R = new string[] {
+    "AGG", "AGA", "CGG", "CGG", "CGA", "CGA", "CGT", "CGT", "CGT", "CGT", "CGT", "CGT", "CGT", "CGT", "CGT", "CGT", "CGT",
+    "CGT", "CGT", "CGT", "CGT", "CGT", "CGT", "CGT", "CGT", "CGC", "CGC", "CGC", "CGC", "CGC", "CGC", "CGC",
+    "CGC", "CGC", "CGC", "CGC", "CGC", "CGC", "CGC", "CGC", "CGC"
+    };
+
+        public static string[] codon_S = new string[] { "AGT", "AGC", "AGC", "TCG", "TCA", "TCT", "TCC" };
+
+        public static string[] codon_K = new string[] { "AAG", "AAA", "AAA", "AAA" };
+
+        public static string[] codon_N = new string[] { "AAT", "AAC" };
+
+        public static string[] codon_I = new string[] {
+    "ATA", "ATC", "ATC", "ATC", "ATC", "ATT", "ATT", "ATT", "ATT", "ATT"
+    };
+
+        public static string[] codon_T = new string[] {
+    "ACG", "ACG", "ACG", "ACA", "ACT", "ACT", "ACC", "ACC", "ACC", "ACC"
+    };
+
+        public static string[] codon_C = new string[] { "TGT", "TGC" };
+
+        public static string[] codon_Y = new string[] { "TAT", "TAT", "TAT", "TAC", "TAC" };
+
+        public static string[] codon_L = new string[] {
+    "TTG", "TTA", "CTG", "CTG", "CTG", "CTG", "CTG", "CTA", "CTT", "CTC"
+    };
+
+        public static string[] codon_F = new string[] { "TTT", "TTC" };
+
+        public static string[] codon_Q = new string[] { "CAG", "CAG", "CAA" };
+
+        public static string[] codon_H = new string[] {
+    "CAT", "CAT", "CAT", "CAT", "CAC", "CAC", "CAC"
+    };
+
+        public static string[] codon_P = new string[] {
+    "CCG", "CCG", "CCG", "CCG", "CCG", "CCA", "CCA", "CCT", "CCT", "CCC"
+    };
+
+        public static string[] codon_M = new string[] { "ATG" };
+
+        public static string[] codon_W = new string[] { "TGG" };
+    }
+}
